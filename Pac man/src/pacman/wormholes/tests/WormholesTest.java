a0614cc375ff6242e9fa7377a3584111fd8b7a88
imran <imran@192.168.0.242>
2021-05-15 17:23:24 +0200

    In juiste packages gezet

--- Pac man/src/pacman/wormholes/tests/WormholesTest.java
+++ Pac man/src/pacman/wormholes/tests/WormholesTest.java
@@ -1,4 +1,4 @@
-package pacman.wormholes;
+package pacman.wormholes.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 
 import pacman.MazeMap;
 import pacman.Square;
+import pacman.wormholes.ArrivalPortal;
+import pacman.wormholes.DeparturePortal;
+import pacman.wormholes.Wormhole;
 
 class WormholesTest {
 
@@ -27,24 +30,24 @@ class WormholesTest {
 		Square square1 = new Square(2, 3, true, mazeMap);
 		Square square2 = new Square(1, 1, true, mazeMap);
 		
-//		ArrivalPortal portalA = new ArrivalPortal(square0);
-//		DeparturePortal portalD = new DeparturePortal(square1);
-//		DeparturePortal newPortalD = new DeparturePortal(square2);
-//		
-//		Wormhole hole = new Wormhole(portalD, portalA);
-//		
-//		assert(portalA.getWormholes().contains(hole) == true);
-//		assert(portalD.getWormholes().contains(hole) == true);
-//		
-//		assert(hole.getArrivalPortal() == portalA);
-//		assert(hole.getDeparturePortal() == portalD);
-//		
-//		hole.setDeparturePortal(newPortalD);
-//		
-//		assert(portalD.getWormholes().isEmpty() == true);
-//		assert(newPortalD.getWormholes().contains(hole) == true);
-//		assert(hole.getDeparturePortal() == newPortalD);
-//		
+		ArrivalPortal portalA = new ArrivalPortal(square0);
+		DeparturePortal portalD = new DeparturePortal(square1);
+		DeparturePortal newPortalD = new DeparturePortal(square2);
+		
+		Wormhole hole = new Wormhole(portalD, portalA);
+		
+		assert(portalA.getWormholes().contains(hole) == true);
+		assert(portalD.getWormholes().contains(hole) == true);
+		
+		assert(hole.getArrivalPortal() == portalA);
+		assert(hole.getDeparturePortal() == portalD);
+		
+		hole.setDeparturePortal(newPortalD);
+		
+		assert(portalD.getWormholes().isEmpty() == true);
+		assert(newPortalD.getWormholes().contains(hole) == true);
+		assert(hole.getDeparturePortal() == newPortalD);
+		
 		ArrivalPortal[] newArrivalPortals = {new ArrivalPortal(square2), new ArrivalPortal(square1), new ArrivalPortal(square0)};
 		
 		for(int j = 0 ; j <= newArrivalPortals.length - 2; j++) {
